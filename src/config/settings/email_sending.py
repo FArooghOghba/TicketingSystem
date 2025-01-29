081c940047ce9ea395c8443224049f33d4147a3f
FArooghOghba <faroogh.oghba@gmail.com>
2025-01-29 20:14:46 +0330

    feat(email-sending): Add JWT max age setting for registration emails
    Added a `DEFAULT_REGISTRATION_EMAIL_JWT_MAX_AGE` setting to
    `email_sending.py` to configure the maximum age of JWTs used in
    registration emails.  This allows for more fine-grained control
    over the lifespan of these tokens.  The default is set to 30 minutes.

--- src/config/settings/email_sending.py
+++ src/config/settings/email_sending.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from config.env import env
 
 
@@ -7,5 +9,7 @@ DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="noreply@example.com")
 DEFAULT_REGISTRATION_EMAIL_SUBJECT = "Welcome to Our Service!"
 DEFAULT_REGISTRATION_EMAIL_TEMPLATE_TXT = "emails/registration_email.txt"
 DEFAULT_REGISTRATION_EMAIL_TEMPLATE_HTML = "emails/registration_email.html"
+DEFAULT_REGISTRATION_EMAIL_JWT_MAX_AGE = timedelta(minutes=30)
+
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
